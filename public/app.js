3332a977ee2292feefd4256853119439ad2b792c
pencte <zakyyulyansyah@gmail.com>
2026-04-06 14:00:16 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,3 @@
-
 /* NAV */
 function show(id){
   document.querySelectorAll(".page").forEach(p=>p.classList.add("hidden"));
@@ -8,7 +7,37 @@ function show(id){
   event.target.classList.add("active");
 }
 
-/* FAKE STATUS (sementara) */
+/* HOW TAB */
+function showHow(type){
+  document.querySelectorAll(".howContent")
+    .forEach(el=>el.classList.add("hidden"));
+
+  document.getElementById(type)
+    .classList.remove("hidden");
+
+  document.querySelectorAll(".tab")
+    .forEach(t=>t.classList.remove("active"));
+
+  event.target.classList.add("active");
+}
+
+/* COPY */
+function copyText(type){
+  let text = "";
+
+  if(type === "PTUNNEL_URL"){
+    text = "https://gtpshost.com/raw/GrowtopiaFS";
+  }
+
+  if(type === "SURGE_URL"){
+    text = "https://gtpshost.com/raw/GrowtopiaFS";
+  }
+
+  navigator.clipboard.writeText(text);
+  alert("Copied: " + text);
+}
+
+/* FAKE STATUS */
 function fakeStatus(){
   let players = Math.floor(Math.random()*100);
 
@@ -18,7 +47,7 @@ function fakeStatus(){
 
 setInterval(fakeStatus,2000);
 
-/* PLAY BUTTON */
+/* PLAY */
 document.querySelector(".play").onclick = ()=>{
   alert("Launcher coming soon...");
 };
