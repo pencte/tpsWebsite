713039240c7657170f5b9fff670c743894a9a643
pencte <zakyyulyansyah@gmail.com>
2026-04-06 14:36:13 +0700

    Update terorismeps.js

--- api/raw/terorismeps.js
+++ api/raw/terorismeps.js
@@ -1,1 +1,38 @@
+export default function handler(req, res) {
+  const ua = req.headers["user-agent"] || "";
 
+  let host = "";
+
+  /* ANDROID */
+  if (ua.includes("Android")) {
+    host = `
+103.59.161.68 growtopia1.com
+103.59.161.68 growtopia2.com
+103.59.161.68 www.growtopia1.com
+103.59.161.68 www.growtopia2.com
+# Android Config
+`;
+  }
+
+  /* IOS */
+  else if (ua.includes("iPhone") || ua.includes("iPad")) {
+    host = `
+103.59.161.68 growtopia1.com
+103.59.161.68 growtopia2.com
+# iOS Surge Config
+`;
+  }
+
+  /* WINDOWS / OTHER */
+  else {
+    host = `
+103.59.161.68 growtopia1.com
+103.59.161.68 growtopia2.com
+# Windows Config
+`;
+  }
+
+  res.setHeader("Content-Type", "text/plain");
+  res.setHeader("Content-Disposition", "inline");
+  res.status(200).send(host);
+}
